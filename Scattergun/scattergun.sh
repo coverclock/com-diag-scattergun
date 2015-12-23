53659ac814bf3693748040c2dea8f1c3f26f56a8
Chip Overclock <coverclock@diag.com>
2015-12-23 10:17:46 -0700

    [1] Added comments to scattergun.sh. [2] Tested on silver using TrueRNG.

--- Scattergun/scattergun.sh
+++ Scattergun/scattergun.sh
@@ -2,6 +2,7 @@
 # Copyright 2015 Digital Aggregates Corporation, Colorado, USA.
 # "Digital Aggregates Corporation" is a registered trademark.
 # Licensed under the terms of the GNU GPL v2.
+
 # sudo /etc/init.d/rng-tools [ start | stop ]
 
 ZERO="`basename $0`"
@@ -20,7 +21,7 @@ ls -l /dev/intel_rng
 ls -l /dev/i810_rng
 ls -l /dev/TrueRNG
 
-ps -ef | grep rngd
+pgrep -a rngd
 
 for STEP in 0 1 2 3 4 5 6 7 8 9; do
 	echo -n "${STEP}: "
@@ -30,6 +31,14 @@ done
 
 time dd if=${SOURCE} of=/dev/null bs=1024 count=1 iflag=fullblock
 
+# sudo apt-get install rng-tools
+
 time ( cat ${SOURCE} | rngtest -c 1000 )
 
-time ( cat ${SOURCE} | rawtoppm -rgb 256 256 | pnmtopng > ${ZERO}-${STAMP}.png )
+# sudo apt-get install ent
+
+time ( FILE="`mktemp ./${ZERO}.XXXXXXXXXX`"; dd if=${SOURCE} of=${FILE} bs=1024 count=128 iflag=fullblock; ent ${FILE}; rm -f ${FILE} )
+
+# sudo apt-get install netpbm
+
+time ( FILE="`mktemp ./${ZERO}.XXXXXXXXXX`"; cat ${SOURCE} | rawtoppm -rgb 256 256 | pnmtopng > ${FILE}; mv -f ${FILE} ${ZERO}-${STAMP}.png )
