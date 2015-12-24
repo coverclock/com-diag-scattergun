703108091e5a837478fed1d32f1a55ecd64a38ee
Chip Overclock <coverclock@diag.com>
2015-12-24 11:04:25 -0700

    Added notes.

--- Scattergun/scattergun.sh
+++ Scattergun/scattergun.sh
@@ -14,11 +14,6 @@ lsb_release -a
 
 ls -l /dev/random
 ls -l /dev/urandom
-ls -l /dev/hwrng
-ls -l /dev/hw_random
-ls -l /dev/hwrandom
-ls -l /dev/intel_rng
-ls -l /dev/i810_rng
 
 pgrep -a rngd
 
@@ -41,10 +36,13 @@ test -x /usr/bin/rngtest && ( cat ${SOURCE} | time rngtest -c 1000 )
 
 # sudo apt-get install ent
 
-test -x /usr/bin/ent && ( FILE=$(mktemp ./${ZERO}.XXXXXXXXXX); dd if=${SOURCE} of=${FILE} bs=1024 count=128 iflag=fullblock; time ent ${FILE}; rm -f ${FILE} )
+test -x /usr/bin/ent && ( FILE=$(mktemp ./${ZERO}-ent.XXXXXXXXXX); dd if=${SOURCE} of=${FILE} bs=1024 count=128 iflag=fullblock; time ent ${FILE} )
 
 # sudo apt-get install netpbm
 
-test -x /usr/bin/rawtoppm && test -x /usr/bin/pnmtopng && ( FILE=$(mktemp ./${ZERO}.XXXXXXXXXX); cat ${SOURCE} | time rawtoppm -rgb 256 256 | pnmtopng > ${FILE}; mv -f ${FILE} ${ZERO}-${STAMP}.png )
+test -x /usr/bin/rawtoppm && test -x /usr/bin/pnmtopng && ( FILE=$(mktemp ./${ZERO}-rawtoppm.XXXXXXXXXX); cat ${SOURCE} | time rawtoppm -rgb 256 256 | pnmtopng > ${FILE}; mv -f ${FILE} ${FILE}.png )
 
 # git clone http://github.com/usnistgov/SP800-90B_EntropyAssessment
+
+NISTPATH=$(dirname $(which iid_main.py))
+test -f ${NISTPATH}/iid_main.py && ( FILE=$(mktemp $(pwd)/{ZERO}-sp800.XXXXXXXX; dd if=/dev/random of=${FILE} bs=1000 count=1000 iflag=fullblock; cd ${NISTPATH}; python iid_main.py ${FILE} 8 1000 -v; python noniid_main.py ${FILE} 8 -v )
