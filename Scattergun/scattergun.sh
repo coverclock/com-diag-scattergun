c8ca611d8b06f3a3d38a7bb1f5791af7daabbeb8
Chip Overclock <coverclock@diag.com>
2015-12-26 13:30:32 -0700

    Minor tweaks.

--- Scattergun/scattergun.sh
+++ Scattergun/scattergun.sh
@@ -3,21 +3,15 @@
 # "Digital Aggregates Corporation" is a registered trademark.
 # Licensed under the terms of the GNU GPL v2.
 
-# sudo /etc/init.d/rng-tools [ start | stop ]
-
 ZERO=$(basename $0)
 SOURCE=${1:-"/dev/random"}
-STAMP=$(date -u +%Y%m%dT%H%M%S.%N%:z)
+STAMP=$(date -u +%Y%m%dT%H%M%S%N%z)
 
 uname -a
 lsb_release -a
 
 ls -l /dev/random
 ls -l /dev/urandom
-
-pgrep -a rngd
-
-test -r /etc/default/rng-tools && ( . /etc/default/rng-tools && echo HRNGDEVICE=${HRNGDEVICE} && ls -l ${HRNGDEVICE} )
 
 for STEP in 0 1 2 3 4 5 6 7 8 9; do
 	echo -n "${STEP}: "
@@ -31,18 +25,51 @@ time dd if=${SOURCE} of=/dev/null bs=${BLOCKSIZE} count=1 iflag=fullblock
 time dd if=${SOURCE} of=/dev/null bs=${BLOCKSIZE} count=1 iflag=fullblock
 
 # sudo apt-get install rng-tools
+# ${EDITOR} /etc/default/rng-tools
+# sudo /etc/init.d/rng-tools start
 
-test -x /usr/bin/rngtest && ( cat ${SOURCE} | time rngtest -c 1000 )
+pgrep -a rngd
+
+if [[ -r /etc/default/rng-tools ]]; then
+	. /etc/default/rng-tools
+	echo HRNGDEVICE=${HRNGDEVICE}
+	ls -l ${HRNGDEVICE}
+fi
+
+if [[ -x /usr/bin/rngtest ]]; then
+	cat ${SOURCE} | time rngtest -c 1000
+fi
 
 # sudo apt-get install ent
 
-test -x /usr/bin/ent && ( FILE=$(mktemp ./${ZERO}-ent.XXXXXXXXXX); dd if=${SOURCE} of=${FILE} bs=1024 count=128 iflag=fullblock; time ent ${FILE} )
+if [[ -x /usr/bin/ent ]]; then
+	FILE=$(mktemp ./${ZERO}-ent-${STAMP}.XXXXXXXXXX)
+	time dd if=${SOURCE} of=${FILE} bs=1024 count=128 iflag=fullblock
+	mv -f ${FILE} ${FILE}.dat
+	time ent ${FILE}.dat
+fi
 
 # sudo apt-get install netpbm
 
-test -x /usr/bin/rawtoppm && test -x /usr/bin/pnmtopng && ( FILE=$(mktemp ./${ZERO}-rawtoppm.XXXXXXXXXX); cat ${SOURCE} | time rawtoppm -rgb 256 256 | pnmtopng > ${FILE}; mv -f ${FILE} ${FILE}.png )
+if [[ ! -x /usr/bin/rawtoppm ]]; then
+	:
+elif [[ ! -x /usr/bin/pnmtopng ]]; then
+	:
+else
+	FILE=$(mktemp ./${ZERO}-rawtoppm-${STAMP}.XXXXXXXXXX)
+	cat ${SOURCE} | time rawtoppm -rgb 256 256 | pnmtopng > ${FILE}
+	mv -f ${FILE} ${FILE}.png
+fi
 
 # git clone http://github.com/usnistgov/SP800-90B_EntropyAssessment
+# export PATH=$PATH:$(pwd)/SP800-90B_EntropyAssessment
 
-NISTPATH=$(dirname $(which iid_main.py))
-test -f ${NISTPATH}/iid_main.py && ( FILE=$(mktemp $(pwd)/{ZERO}-sp800.XXXXXXXX; dd if=/dev/random of=${FILE} bs=1000 count=1000 iflag=fullblock; cd ${NISTPATH}; python iid_main.py ${FILE} 8 1000 -v; python noniid_main.py ${FILE} 8 -v )
+NISTCODE=$(which iid_main.py)
+if [[ ! -z "${NISTCODE}" ]]; then
+	NISTPATH=$(dirname ${NISTCODE})
+	FILE=$(mktemp $(pwd)/${ZERO}-sp800-${STAMP}.XXXXXXXXXX)
+	time dd if=/dev/random of=${FILE} bs=1000 count=1000 iflag=fullblock
+	mv -f ${FILE} ${FILE}.dat
+	( cd ${NISTPATH}; time python iid_main.py ${FILE}.dat 8 1000 -v )
+	( cd ${NISTPATH}; time python noniid_main.py ${FILE}.dat 8 -v )
+fi
