00a234232f9ae8965b610b4fd7217a36b7ec8d6b
Chip Overclock <coverclock@diag.com>
2015-12-26 14:24:40 -0700

    Working on getting it to run on the RPi 2 B.

--- Scattergun/scattergun.sh
+++ Scattergun/scattergun.sh
@@ -12,6 +12,8 @@ lsb_release -a
 
 ls -l /dev/random
 ls -l /dev/urandom
+ls -l /dev/hwrng
+ls -l /dev/TrueRNG
 
 for STEP in 0 1 2 3 4 5 6 7 8 9; do
 	echo -n "${STEP}: "
@@ -24,11 +26,15 @@ BLOCKSIZE=$((${POOLSIZE} / 8))
 time dd if=${SOURCE} of=/dev/null bs=${BLOCKSIZE} count=1 iflag=fullblock
 time dd if=${SOURCE} of=/dev/null bs=${BLOCKSIZE} count=1 iflag=fullblock
 
+# modprobe bcm2708_rng # Raspberry Pi 2 Model B v1.1 2014
+
+lsmod | grep bcm2708_rng
+
 # sudo apt-get install rng-tools
 # ${EDITOR} /etc/default/rng-tools
 # sudo /etc/init.d/rng-tools start
 
-pgrep -a rngd
+ps -ef | grep rngd
 
 if [[ -r /etc/default/rng-tools ]]; then
 	. /etc/default/rng-tools
@@ -37,7 +43,7 @@ if [[ -r /etc/default/rng-tools ]]; then
 fi
 
 if [[ -x /usr/bin/rngtest ]]; then
-	cat ${SOURCE} | time rngtest -c 1000
+	cat ${SOURCE} | time /usr/bin/rngtest -c 1000
 fi
 
 # sudo apt-get install ent
@@ -46,7 +52,7 @@ if [[ -x /usr/bin/ent ]]; then
 	FILE=$(mktemp ./${ZERO}-ent-${STAMP}.XXXXXXXXXX)
 	time dd if=${SOURCE} of=${FILE} bs=1024 count=128 iflag=fullblock
 	mv -f ${FILE} ${FILE}.dat
-	time ent ${FILE}.dat
+	time /usr/bin/ent ${FILE}.dat
 fi
 
 # sudo apt-get install netpbm
@@ -57,7 +63,7 @@ elif [[ ! -x /usr/bin/pnmtopng ]]; then
 	:
 else
 	FILE=$(mktemp ./${ZERO}-rawtoppm-${STAMP}.XXXXXXXXXX)
-	cat ${SOURCE} | time rawtoppm -rgb 256 256 | pnmtopng > ${FILE}
+	cat ${SOURCE} | time /usr/bin/rawtoppm -rgb 256 256 | /usr/bin/pnmtopng > ${FILE}
 	mv -f ${FILE} ${FILE}.png
 fi
 
