c3f6aec47f12c6eb5be75364608687d01dd840e2
Chip Overclock <coverclock@diag.com>
2016-03-19 15:22:03 -0600

    [1] Still trying to work out meaningful metrics (esp. peak). [2] Tested on silver.

--- Scattergun/src/rate.c
+++ Scattergun/src/rate.c
@@ -12,6 +12,8 @@
  *
  * USAGE
  *
+ * rate [ 
+ *
  * EXAMPLES
  *
  * ABSTRACT
@@ -30,37 +32,18 @@
 #include <fcntl.h>
 
 static const char * program = "rate";
-static size_t reads = 0;
-static size_t total = 0;
-static int64_t then = 0;
-static int hungup = 0;
 
-/**
- * Handle a signal. In the event of a SIGPIPE or a SIGINT, the program shuts
- * down in an orderly fashion. In the event of a SIGHUP, it emits some
- * statistics to standard error.
- * @param signum is the number of the incoming signal.
- */
-static void handler(int signum)
-{
-    if (signum == SIGHUP) {
-        hungup = !0;
-    } else {
-        /* Do nothing. */
-    }
-}
-
-static int64_t watch(void)
+static uint64_t watch(void)
 {
     int rc;
-    int64_t ticks = -1;
-    struct timespec elapsed;
+    uint64_t ticks = ~0;
+    struct timespec spec;
 
-    rc = clock_gettime(CLOCK_MONOTONIC_RAW, &elapsed);
+    rc = clock_gettime(CLOCK_MONOTONIC_RAW, &spec);
     if (rc == 0) {
-        ticks = elapsed.tv_sec;
+        ticks = spec.tv_sec;
         ticks *= 1000000000;
-        ticks += elapsed.tv_nsec;
+        ticks += spec.tv_nsec;
     } else {
         perror("clock_gettime");
     }
@@ -68,16 +51,13 @@ static int64_t watch(void)
     return ticks;
 }
 
-static void report(void)
+static void usage(void)
 {
-    int64_t elapsed;
-    elapsed = watch() - then;
-    printf("%s: %lu milliseconds\n", program, elapsed / 1000000);
-    printf("%s: %zu bytes\n", program, total);
-    printf("%s: %zu reads\n", program, reads);
-    if ((total == 0) || (reads == 0)) { return; }
-    printf("%s: %zu bytes/read\n", program, total / reads);
-    printf("%s: %zu kilobytes/second\n", program, (total * 1000000) / elapsed);
+    fprintf(stderr, "usage: %s [ -h ] [ -f PATH ] [ -r BYTES ] [ -t BYTES ]\n", program);
+    fprintf(stderr, "       -f PATH     Read from here instead of stdin.\n");
+    fprintf(stderr, "       -r BYTES    Read no more than this at a time.\n");
+    fprintf(stderr, "       -t BYTES    Read no more than this total.\n");
+    fprintf(stderr, "       -h          Display this menu.\n");
 }
 
 /**
@@ -89,23 +69,19 @@ int main(int argc, char * argv[])
 {
     int xc = 1;
     int error = 0;
-    int debug = 0;
-    int verbose = 0;
     size_t size = 4096;
     const char * path = (const char *)0;
     int fd = STDIN_FILENO;
     uint8_t * buffer = (uint8_t *)0;
-    ssize_t bytes = 0;
     size_t limit = ~0;
     int rc = 0;
     char * end = (char *)0;
-    struct sigaction sighup = { 0 };
     int opt;
     extern char * optarg;
 
     program = ((program = strrchr(argv[0], '/')) == (char *)0) ? argv[0] : program + 1;
 
-    while ((opt = getopt(argc, argv, "f:l:r:")) >= 0) {
+    while ((opt = getopt(argc, argv, "f:ht:r:")) >= 0) {
 
         switch (opt) {
 
@@ -113,13 +89,8 @@ int main(int argc, char * argv[])
             path = optarg;
             break;
 
-        case 'l':
-            limit = strtoul(optarg, &end, 0);
-            if (*end != '\0') {
-                errno = EINVAL;
-                perror(optarg);
-                error = !0;
-            }
+        case 'h':
+            usage();
             break;
 
         case 'r':
@@ -131,7 +102,17 @@ int main(int argc, char * argv[])
             }
             break;
 
+        case 't':
+            limit = strtoul(optarg, &end, 0);
+            if (*end != '\0') {
+                errno = EINVAL;
+                perror(optarg);
+                error = !0;
+            }
+            break;
+
         default:
+            usage();
             error = !0;
             break;
 
@@ -144,6 +125,18 @@ int main(int argc, char * argv[])
     }
 
     do {
+        uint64_t epoch = 0;
+        uint64_t then = 0;
+        uint64_t now = 0;
+        uint64_t elapsed = 0;
+        uint64_t duration = 0;
+        ssize_t bytes = 0;
+        size_t reads = 0;
+        size_t total = 0;
+        size_t segment = 0;
+        size_t sustained = 0;
+        size_t peak = 0;
+        size_t burst = 0;
 
         if (error) {
             break;
@@ -155,14 +148,6 @@ int main(int argc, char * argv[])
             break;
         }
 
-        sighup.sa_handler = handler;
-        sighup.sa_flags = SA_RESTART;
-        rc = sigaction(SIGHUP, &sighup, (struct sigaction *)0);
-        if (rc < 0) {
-            perror("sigaction");
-            break;
-        }
-
         if (path != (const char *)0) {
             fd = open(path, O_RDONLY);
             if (fd < 0) {
@@ -171,9 +156,11 @@ int main(int argc, char * argv[])
             }
         }
 
-        then = watch();
+        epoch = watch();
         while (limit > 0) {
 
+            then = now;
+            now = watch();
             bytes = read(fd, buffer, (size < limit) ? size : limit);
             if (bytes < 0) {
                 perror("read");
@@ -182,25 +169,39 @@ int main(int argc, char * argv[])
                 xc = 0;
                 break;
             } else {
-                reads += 1;
-                total += bytes;
-                limit -= bytes;
+                /* Do nothing. */
             }
 
-            if (hungup) {
-                report();
-                hungup = 0;
+            reads += 1;
+            total += bytes;
+            limit -= bytes;
+
+            elapsed = watch() - epoch;
+            sustained = (total * 1000000) / elapsed;
+
+            segment = (total + (reads / 2)) / reads;
+
+            if (reads > 1) {
+                duration = now - then;
+                burst = (bytes * 1000000) / duration;
+                if (burst > peak) {
+                    peak = burst;
+                }
             }
-    
         }
 
         if (close(fd) < 0) {
             perror("close");
         }
 
-    } while (0);
+        printf("%s: %lu milliseconds\n", program, elapsed / 1000000);
+        printf("%s: %zu bytes\n", program, total);
+        printf("%s: %zu reads\n", program, reads);
+        printf("%s: %zu bytes/read\n", program, segment);
+        printf("%s: %zu kilobytes/second sustained\n", program, sustained);
+        printf("%s: %zu kilobytes/second peak\n", program, peak);
 
-    report();
+    } while (0);
 
     return xc;
 }
