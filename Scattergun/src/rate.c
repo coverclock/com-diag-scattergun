b84dfcb96ae4bd9c5522524db9cf5a8b2a77f993
Chip Overclock <coverclock@diag.com>
2016-03-20 15:56:11 -0600

    [1] Still working on metrics. [2] Testing on silver.

--- Scattergun/src/rate.c
+++ Scattergun/src/rate.c
@@ -12,11 +12,24 @@
  *
  * USAGE
  *
- * rate [ 
+ * rate [ -h ] [ -d ] [ -v ] [ -f PATH ] [ -r BYTES ] [ -t BYTES ]
+ *
+ * OPTIONS
+ *
+ * -f PATH     Read from here instead of stdin.
+ * -r BYTES    Read no more than this at a time.
+ * -t BYTES    Read no more than this total.
+ * -h          Display this menu.
+ * -d          Display debug output.
+ * -v          Display verbose output.
  *
  * EXAMPLES
  *
+ * rate -f /dev/TrueRNGpro -r 4096 -t 1000000000
+ *
  * ABSTRACT
+ *
+ * Measures the sustained and peak rates of a data source.
  */
 
 #include <stdlib.h>
@@ -53,11 +66,13 @@ static uint64_t watch(void)
 
 static void usage(void)
 {
-    fprintf(stderr, "usage: %s [ -h ] [ -f PATH ] [ -r BYTES ] [ -t BYTES ]\n", program);
+    fprintf(stderr, "usage: %s [ -d ] [ -f PATH ] [ -h ] [ -r BYTES ] [ -t BYTES ] [ -v ] \n", program);
+    fprintf(stderr, "       -d          Display debug output.\n");
     fprintf(stderr, "       -f PATH     Read from here instead of stdin.\n");
+    fprintf(stderr, "       -h          Display this menu.\n");
     fprintf(stderr, "       -r BYTES    Read no more than this at a time.\n");
     fprintf(stderr, "       -t BYTES    Read no more than this total.\n");
-    fprintf(stderr, "       -h          Display this menu.\n");
+    fprintf(stderr, "       -v          Display verbose output.\n");
 }
 
 /**
@@ -74,16 +89,23 @@ int main(int argc, char * argv[])
     int fd = STDIN_FILENO;
     uint8_t * buffer = (uint8_t *)0;
     size_t limit = ~0;
+    size_t remaining = 0;
     int rc = 0;
     char * end = (char *)0;
+    int debug = 0;
+    int verbose = 0;
     int opt;
     extern char * optarg;
 
     program = ((program = strrchr(argv[0], '/')) == (char *)0) ? argv[0] : program + 1;
 
-    while ((opt = getopt(argc, argv, "f:ht:r:")) >= 0) {
+    while ((opt = getopt(argc, argv, "df:ht:r:v")) >= 0) {
 
         switch (opt) {
+
+        case 'd':
+            debug = !0;
+            break;
 
         case 'f':
             path = optarg;
@@ -111,6 +133,10 @@ int main(int argc, char * argv[])
             }
             break;
 
+        case 'v':
+            verbose = !0;
+            break;
+
         default:
             usage();
             error = !0;
@@ -133,10 +159,13 @@ int main(int argc, char * argv[])
         ssize_t bytes = 0;
         size_t reads = 0;
         size_t total = 0;
-        size_t segment = 0;
         size_t sustained = 0;
-        size_t peak = 0;
         size_t burst = 0;
+        size_t average = 0;
+        size_t minimum = ~0;
+        size_t maximum = 0;
+        size_t low = ~0;
+        size_t peak = 0;
 
         if (error) {
             break;
@@ -156,12 +185,16 @@ int main(int argc, char * argv[])
             }
         }
 
-        epoch = watch();
-        while (limit > 0) {
+        printf("%s: %zu bytes limit\n", program, limit);
+        printf("%s: %zu bytes requested\n", program, size);
 
-            then = now;
+        remaining = limit;
+        epoch = watch();
+        while (remaining >= size) {
+
+            then = watch();
+            bytes = read(fd, buffer, size);
             now = watch();
-            bytes = read(fd, buffer, (size < limit) ? size : limit);
             if (bytes < 0) {
                 perror("read");
                 break;
@@ -174,30 +207,61 @@ int main(int argc, char * argv[])
 
             reads += 1;
             total += bytes;
-            limit -= bytes;
+            remaining -= bytes;
 
-            elapsed = watch() - epoch;
+            average = (total + (reads / 2)) / reads;
+
+            elapsed = now - epoch;
             sustained = (total * 1000000) / elapsed;
 
-            segment = (total + (reads / 2)) / reads;
+            duration = now - then;
+            burst = (bytes * 1000000) / duration;
 
-            if (reads > 1) {
-                duration = now - then;
-                burst = (bytes * 1000000) / duration;
-                if (burst > peak) {
-                    peak = burst;
+            if (debug) {
+                printf("%s: %zu bytes %zu nanoseconds %lu kilobytes/second\n", program, bytes, duration, burst);
+            }
+
+            if (verbose && ((burst > peak) || (burst < low))) {
+                printf("%s: %lu nanoseconds elapsed\n", program, elapsed);
+                printf("%s: %lu nanoseconds burst\n", program, duration);
+                printf("%s: %zu bytes read\n", program, bytes);
+            }
+
+            if (bytes < minimum) {
+                minimum = bytes;
+            }
+
+            if (bytes > maximum) {
+                maximum = bytes;
+            }
+
+            if (burst < low) {
+                low = burst;
+                if (verbose) {
+                    printf("%s: %zu kilobytes/second low\n", program, low);
                 }
             }
+
+            if (burst > peak) {
+                peak = burst;
+                if (verbose) {
+                    printf("%s: %zu kilobytes/second peak\n", program, peak);
+                }
+            }
+
         }
 
         if (close(fd) < 0) {
             perror("close");
         }
 
-        printf("%s: %lu milliseconds\n", program, elapsed / 1000000);
-        printf("%s: %zu bytes\n", program, total);
+        printf("%s: %zu bytes total\n", program, total);
+        printf("%s: %lu milliseconds elapsed\n", program, elapsed / 1000000);
         printf("%s: %zu reads\n", program, reads);
-        printf("%s: %zu bytes/read\n", program, segment);
+        printf("%s: %zu bytes minimum\n", program, minimum);
+        printf("%s: %zu bytes average\n", program, average);
+        printf("%s: %zu bytes maximum\n", program, maximum);
+        printf("%s: %zu kilobytes/second low\n", program, low);
         printf("%s: %zu kilobytes/second sustained\n", program, sustained);
         printf("%s: %zu kilobytes/second peak\n", program, peak);
 
